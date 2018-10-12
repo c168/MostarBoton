82b3025550220cb5767d9747f072e9a8350b8b50
formato-botonOculto <cesarmencia7@gmail.com>
2018-10-11 20:10:45 -0600

    formulario

---
+++ creaar/creaar/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace creaar
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text == "1234")
+            {
+                button2.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("ingrese la contraseña");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido");
+        }
+    }
+}
